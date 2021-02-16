2a91c50313f2441a377f925828a65f10b646825f
Romain Morel <55758791+Fyrius@users.noreply.github.com>
2021-02-16 17:56:10 +0100

    Modification de lecture et d'écrire
    Mosification de la lecture de la liste de symptomes afin qu'elle s'auto complète en fonction du "symptoms.txt"
    
    Modification de la méthode d'écriture dans le "result.txt" afin de fonctionner au mieux avec les nouvelles méthodes
    
    Ajout de la méthode "getKey()" créer par Baeldung afin de pouvoir récuperer la key d'une valeur de la map
    
    Découverte d'un bug dans l'écriture de "result.txt". Certains symptomes sont écrient de façon répétés.

--- src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,54 +1,42 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
-public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
-	
+public class AnalyticsCounter {	
 	private static Map<String, Integer> count = new HashMap<String, Integer>();
+	
+	public static <K, V> K getKey(Map<K, V> map, V value) {
+	    for (Entry<K, V> entry : map.entrySet()) {
+	        if (entry.getValue().equals(value)) {
+	            return entry.getKey();
+	        }
+	    }
+	    return null;
+	}
 	
 	public static void main(String args[]) throws Exception {
 		// first get input
 		List<String> list = new ReadSymptomDataFromFile().getSymptoms("src/symptoms.txt");
 		
-		/*
-		 * 1 - Faire une boucle qui navige dans la list des symptomes
-		 * 2 - Si symptoms pas dans la map alors ajout
-		 * 	   Sinon, get la valeur puis incrémenté de 1
-		 * 
-		*/
-		
-		int i = 0;	// set i to 0
-		while (line != null) {
-			i++;	// increment i
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headacheCount++;
-				System.out.println("number of headaches: " + headacheCount);
+		for(int i = 0; i < list.size(); i++) {
+			String key = list.get(i);
+					
+			if(count.containsKey(key)) {
+				count.replace(key, count.get(key) + 1);
+			}else {
+				count.put(key, 1);
 			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
-
-			line = reader.readLine();	// get another symptom
 		}
-		
-		
+				
 		// next generate output
 		FileWriter writer = new FileWriter ("src/result.txt");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
+		for(Integer i : count.values()) {
+			writer.write(getKey(count, i) + ": " + i + "\n");
+		}
 		writer.close();
 	}
 }
