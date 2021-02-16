d5fbda41180266942817c34f0d9f404a7bc6257b
Romain Morel <55758791+Fyrius@users.noreply.github.com>
2021-02-16 18:18:57 +0100

    Premier test de correction
    Premier test de correction lors de l'écriture. Le bug de duplication n'est pas de la duplication. Il s'agit juste d'un soucis de detection de la key lorsque plusieur clef on une valeur égale

--- src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -4,20 +4,19 @@ import java.io.FileWriter;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
+import java.util.stream.Stream;
 
 public class AnalyticsCounter {	
 	private static Map<String, Integer> count = new HashMap<String, Integer>();
 	
-	public static <K, V> K getKey(Map<K, V> map, V value) {
-	    for (Entry<K, V> entry : map.entrySet()) {
-	        if (entry.getValue().equals(value)) {
-	            return entry.getKey();
-	        }
-	    }
-	    return null;
+	public static <K, V> Stream<K> getKeys(Map<K, V> map, V value) {
+	    return map
+	      .entrySet()
+	      .stream()
+	      .filter(entry -> value.equals(entry.getValue()))
+	      .map(Map.Entry::getKey);
 	}
-	
+		
 	public static void main(String args[]) throws Exception {
 		// first get input
 		List<String> list = new ReadSymptomDataFromFile().getSymptoms("src/symptoms.txt");
@@ -32,10 +31,13 @@ public class AnalyticsCounter {
 			}
 		}
 				
+		System.out.println(count.toString());
+		System.out.println(count.size());
+		
 		// next generate output
 		FileWriter writer = new FileWriter ("src/result.txt");
 		for(Integer i : count.values()) {
-			writer.write(getKey(count, i) + ": " + i + "\n");
+			writer.write(getKeys(count, i) + ": " + i + "\n");
 		}
 		writer.close();
 	}
