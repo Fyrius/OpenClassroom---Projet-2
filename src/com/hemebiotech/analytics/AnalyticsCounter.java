316ac300761773e140549560a941a4bb2366da78
Romain Morel <55758791+Fyrius@users.noreply.github.com>
2021-02-18 18:44:51 +0100

    Correction écriture et ajout du tri
    Correction de la fonction écriture afin que ça affiche le format demander
    
    Ajout de la fonction de tri qui ordonne la hashmap par ordre alphabétique

--- src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -4,23 +4,18 @@ import java.io.FileWriter;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Stream;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.TreeMap;
 
 public class AnalyticsCounter {	
 	private static Map<String, Integer> count = new HashMap<String, Integer>();
 	
-	public static <K, V> Stream<K> getKeys(Map<K, V> map, V value) {
-	    return map
-	      .entrySet()
-	      .stream()
-	      .filter(entry -> value.equals(entry.getValue()))
-	      .map(Map.Entry::getKey);
-	}
-		
 	public static void main(String args[]) throws Exception {
 		// first get input
 		List<String> list = new ReadSymptomDataFromFile().getSymptoms("src/symptoms.txt");
 		
+		// comptage
 		for(int i = 0; i < list.size(); i++) {
 			String key = list.get(i);
 					
@@ -30,15 +25,24 @@ public class AnalyticsCounter {
 				count.put(key, 1);
 			}
 		}
-				
-		System.out.println(count.toString());
-		System.out.println(count.size());
+		
+		// tri
+		TreeMap<String, Integer> sorted = new TreeMap<>(count);
+        Set<Entry<String, Integer>> mappings = sorted.entrySet();
+        
+        System.out.println("HashMap after sorting by keys in ascending order ");
+        for(Entry<String, Integer> mapping : mappings){
+            System.out.println(mapping.getKey() + " ==> " + mapping.getValue());
+        }
 		
 		// next generate output
 		FileWriter writer = new FileWriter ("src/result.txt");
-		for(Integer i : count.values()) {
-			writer.write(getKeys(count, i) + ": " + i + "\n");
-		}
+		/*for (String key: count.keySet()) {
+			writer.write(key + ": " + count.get(key) + "\n");
+	    }*/
+		for (Entry<String, Integer> entry: mappings) {
+			writer.write(entry.getKey() + ": " + entry.getValue() + "\n");
+	    }
 		writer.close();
 	}
 }
